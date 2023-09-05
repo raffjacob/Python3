01520e768e4505273ad79211d1ed9d745c933ba9
raffjacob <rafael.jacob301@gmail.com>
2023-09-05 19:14:45 -0300

    created EX ListaDeCompras

---
+++ Aulas/exercicios/S3L90_ListaDeCompras.py
@@ -0,0 +1,16 @@
+"""
+Faça uma lista de compras usando list
+O usuário deve ter a possibilidade de inserir, apagar e listar valores
+Não permita que o programa quebre com erros de índices inexistentes na lista.
+"""
+
+import os
+
+while True:
+    os.system("cls")
+    print("Lista de Compras")
+    print("Opções: [I]nserir [A]pagar [L]istar")
+    opcao = input("Insira uma opção: ")
+    #if opcao == "A" then:
+
+
